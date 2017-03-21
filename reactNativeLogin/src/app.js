cb867f65473f8d0625298372a49d190b2ec4a3bb
Andrew Chung <andrewjung.euihyun@gmail.com>
2017-03-21 15:39:51 -0700

    set up eslint, set up firebase config

--- reactNativeLogin/src/app.js
+++ reactNativeLogin/src/app.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common/index';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCc7gicqo741H_RnKdnI0KssHEA6II4kyI',
+      authDomain: 'reactnativelogin.firebaseapp.com',
+      databaseURL: 'https://reactnativelogin.firebaseio.com',
+      storageBucket: 'reactnativelogin.appspot.com',
+      messagingSenderId: '331972565453'
+    });
+  }
+
   render() {
     return (
       <View>
